53f5c928c55c5e3953228e1ec7126421d778d199
Urcola Gonzalo <gonzalomup@gmail.com>
2022-10-12 19:48:28 -0300

    cambios

--- proyectoFamilias/views.py
+++ proyectoFamilias/views.py
@@ -2,12 +2,12 @@ from random import Random, random
 from re import template
 from django.http import HttpResponse
 from datetime import date, datetime
-from django.template import context, Template, loader
+from django.template import Context, Template, loader
 from Familia.models import familiares
 
 def crear_familiar (request,nombre,apellido,edad):
     familia= familiares(nombre=nombre,apellido=apellido,edad=edad)
-    familiares.save()
+    familiares.save(nombre,apellido,edad)
 
 
     cargar_archivo= open(r'C:\Users\Tango\Desktop\Familia-Proyecto\Templates\crear_familiar.html','r')

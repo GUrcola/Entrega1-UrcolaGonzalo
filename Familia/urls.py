ea682ecbf3304c21acb6b9744fbbc65af934889f
Urcola Gonzalo <gonzalomup@gmail.com>
2022-10-14 19:22:51 -0300

    1 entrega

--- Familia/urls.py
+++ Familia/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 urlpatterns = [
     path('', views.index, name='Index'), 
     path('familiares/', views.ver_familia, name='ver familia'),
-    path('crear-familiar/<str:nombre>/<str:apellido>/<int:edad>/', views.crear_familiar, name='crear familiar'),
+    path('crear-familiar/', views.crear_familiar, name='crear familiar'),
     path('about/', views.About, name='About')
  ]
--- Familia/views.py
+++ Familia/views.py
@@ -1,17 +1,23 @@
+from contextlib import redirect_stderr
 from re import template
 from django.http import HttpResponse
 from django.template import Context, Template
 # loader
 from Familia.models import familiares
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-def crear_familiar (request,nombre,apellido,edad):
-    familia= familiares(nombre=nombre,apellido=apellido,edad=edad)
-    familia.save()
+def crear_familiar (request):
+    if request.method =='POST':
+        nombre= request.POST.get('nombre')
+        apellido= request.POST.get('apellido')
+        familia= familiares(nombre=nombre,apellido=apellido)
+        familia.save()
+        return redirect('ver familia')    
+        
     # template= loader.get_template('crear_familiar.html')
     # template_renderizado= template.render()
     # return HttpResponse (template_renderizado)  
-    return render(request,'Familia/crear_familiar.html',{'familia': familia})
+    return render(request,'Familia/crear_familiar.html',{})
 
 
 
